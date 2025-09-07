f7f4c41d855b053a01b7fa6e0f4a0db8c411ed33
Samuel Georges <sam@daftspunk.com>
2025-09-07 13:13:02 +1000

    Spawn composer package (2.0)

---
+++ src/Contracts/ViewComponentInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace October\Ajax\Contracts;
+
+/**
+ * AjaxComponentInterface
+ */
+interface ViewComponentInterface
+{
+    /**
+     * newInstance returns a new instance of an AJAX component
+     */
+    public static function createIn(AjaxControllerInterface $controller, array $config): AjaxComponentInterface;
+
+    /**
+     * bindToController is called when the component has been fully prepared
+     */
+    public function bindToController();
+}
