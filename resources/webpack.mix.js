7fefdb37d490c3d985ef1ebc2304b3a4b3543017
Samuel Georges <sam@daftspunk.com>
2025-12-06 18:22:33 +1100

    Remove extras and turbo bundles

--- resources/webpack.mix.js
+++ resources/webpack.mix.js
@@ -19,13 +19,9 @@ mix
     });
 
 mix.js('src/framework-bundle.js', 'dist/framework-bundle.min.js');
-mix.js('src/framework-extras.js', 'dist/framework-extras.min.js');
-mix.js('src/framework-turbo.js', 'dist/framework-turbo.min.js');
 mix.js('src/framework.js', 'dist/framework.min.js');
 
 if (!mix.inProduction()) {
     mix.js('src/framework-bundle.js', 'dist/framework-bundle.js');
-    mix.js('src/framework-extras.js', 'dist/framework-extras.js');
-    mix.js('src/framework-turbo.js', 'dist/framework-turbo.js');
     mix.js('src/framework.js', 'dist/framework.js');
 }
