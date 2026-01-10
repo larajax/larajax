ef06ab60f3ed0478291c181db13c9a72a317f3da
Sam G <sam@daftspunk.com>
2026-01-10 17:45:41 +1100

    Rebuild

--- resources/dist/framework-bundle.js
+++ resources/dist/framework-bundle.js
@@ -1,29 +1,7 @@
-var jax = (() => {
+(() => {
   var __defProp = Object.defineProperty;
-  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __hasOwnProp = Object.prototype.hasOwnProperty;
   var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-  var __export = (target, all) => {
-    for (var name in all)
-      __defProp(target, name, { get: all[name], enumerable: true });
-  };
-  var __copyProps = (to, from, except, desc) => {
-    if (from && typeof from === "object" || typeof from === "function") {
-      for (let key of __getOwnPropNames(from))
-        if (!__hasOwnProp.call(to, key) && key !== except)
-          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-    }
-    return to;
-  };
-  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
   var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
-
-  // src/framework-bundle.js
-  var framework_bundle_exports = {};
-  __export(framework_bundle_exports, {
-    default: () => framework_bundle_default
-  });
 
   // src/util/index.js
   function dispatch(eventName, { target = document, detail = {}, bubbles = true, cancelable = true } = {}) {
@@ -5956,6 +5934,4 @@ var jax = (() => {
   namespace_default3.start();
   namespace_default4.start();
   namespace_default5.start();
-  var framework_bundle_default = window.jax;
-  return __toCommonJS(framework_bundle_exports);
 })();
--- resources/dist/framework.js
+++ resources/dist/framework.js
@@ -1,29 +1,7 @@
-var jax = (() => {
+(() => {
   var __defProp = Object.defineProperty;
-  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __hasOwnProp = Object.prototype.hasOwnProperty;
   var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-  var __export = (target, all) => {
-    for (var name in all)
-      __defProp(target, name, { get: all[name], enumerable: true });
-  };
-  var __copyProps = (to, from, except, desc) => {
-    if (from && typeof from === "object" || typeof from === "function") {
-      for (let key of __getOwnPropNames(from))
-        if (!__hasOwnProp.call(to, key) && key !== except)
-          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-    }
-    return to;
-  };
-  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
   var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
-
-  // src/framework.js
-  var framework_exports = {};
-  __export(framework_exports, {
-    default: () => framework_default
-  });
 
   // src/util/index.js
   function dispatch(eventName, { target = document, detail = {}, bubbles = true, cancelable = true } = {}) {
@@ -2903,6 +2881,4 @@ var jax = (() => {
     visit: (url) => window.location.assign(url)
   }));
   namespace_default2.start();
-  var framework_default = window.jax;
-  return __toCommonJS(framework_exports);
 })();
