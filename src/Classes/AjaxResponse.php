0c22e4a26b8c48771f2d6e15037c65f9892256f6
Samuel Georges <sam@daftspunk.com>
2026-03-05 22:33:09 +1100

    Fixes using ['defer'] as attributes (regression)
    Firefox throws DOMException when asset attributes are passed as sequential array

--- src/Classes/AjaxResponse.php
+++ src/Classes/AjaxResponse.php
@@ -353,6 +353,8 @@ class AjaxResponse implements Responsable
      */
     protected function normalizeAssetPaths(string|array $paths, array $attributes = []): array
     {
+        $attributes = $this->normalizeAssetAttributes($attributes);
+
         // Single string path
         if (is_string($paths)) {
             return $attributes ? [['url' => $paths, 'attributes' => $attributes]] : [$paths];
@@ -363,7 +365,7 @@ class AjaxResponse implements Responsable
         foreach ($paths as $key => $value) {
             // Associative: path => attributes
             if (is_string($key)) {
-                $attrs = is_array($value) ? $value : [];
+                $attrs = is_array($value) ? $this->normalizeAssetAttributes($value) : [];
                 $assets[] = $attrs ? ['url' => $key, 'attributes' => $attrs] : $key;
             }
             // Sequential: just a path string
@@ -373,6 +375,25 @@ class AjaxResponse implements Responsable
         }
 
         return $assets;
+    }
+
+    /**
+     * Normalize sequential attribute arrays (['defer']) to associative (['defer' => true]).
+     */
+    protected function normalizeAssetAttributes(array $attributes): array
+    {
+        $normalized = [];
+
+        foreach ($attributes as $key => $value) {
+            if (is_int($key) && is_string($value)) {
+                $normalized[$value] = true;
+            }
+            else {
+                $normalized[$key] = $value;
+            }
+        }
+
+        return $normalized;
     }
 
     /**
